4889a234ddc5b1007a6cccdf7768b9ce4d399861
Thomas Mailund <mailund@birc.au.dk>
2022-09-14 12:31:47 +0200

    fixed broken tests

--- src/ba.py
+++ src/ba.py
@@ -12,7 +12,13 @@ def border_array(x: str) -> list[int]:
     >>> border_array("")
     []
     """
-    return []  # FIXME
+    ba = [0] * len(x)
+    for j in range(1, len(x)):
+        b = ba[j - 1]
+        while b > 0 and x[j] != x[b]:
+            b = ba[b - 1]
+        ba[j] = b + 1 if x[j] == x[b] else 0
+    return ba
 
 
 def strict_border_array(x: str) -> list[int]:
@@ -34,4 +40,8 @@ def strict_border_array(x: str) -> list[int]:
     >>> strict_border_array("")
     []
     """
-    return []  # FIXME
+    ba = border_array(x)
+    for j, b in enumerate(ba[:-1]):
+        if x[b] == x[j + 1] and b > 0:
+            ba[j] = ba[b - 1]
+    return ba
